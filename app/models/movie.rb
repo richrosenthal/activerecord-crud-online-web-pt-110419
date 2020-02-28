c1ed4f1dedb9403a762792ace11c65f20461fc62
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-28 17:33:50 +0000

    Automatically backed up by Learn

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
   
   def can_be_instantiated_and_then_saved
-    movie = Movie.New 
+    movie = Movie.new 
     movie.title = "This is a title"
     movie.save
   end 
