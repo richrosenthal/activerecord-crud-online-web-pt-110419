9e6638955eb788c8d52c46b04305209641d539c0
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-28 17:22:24 +0000

    Automatically backed up by Learn

--- db/migrate/20200228171828_create_movies.rb
+++ db/migrate/20200228171828_create_movies.rb
@@ -1,4 +1,9 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
+     create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
   end
 end
